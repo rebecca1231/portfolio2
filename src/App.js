db5d72351fe250e880904f7a93d8c10f3b2b58e6
Rebecca <rebeccagetsanemail@gmail.com>
2020-09-06 13:26:20 +0900

    initial commit boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from './Components/Header'
+import Footer from './Components/Footer';
+import Landing from './Components/Landing';
+import Contact from './Components/Contact';
+import About from './Components/About'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
