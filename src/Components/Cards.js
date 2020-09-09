dff6aa301c70358b3479e073ccaa0a2a62d27f74
Rebecca <rebeccagetsanemail@gmail.com>
2020-09-10 06:20:36 +0900

    added project image files

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Cards.css";
 
+//make an array of objects 
+//name, description, link, pictures
+//  div picture / link (a)
+//on hover: picture goes white-ish, white overlay?  and new div with title, dexcription show up
+
+
 const Cards = () => {
   return (
     <div>
