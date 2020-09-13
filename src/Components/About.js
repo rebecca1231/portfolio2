b55835ef80034e0a0b4774b234cea1564429c4a4
Rebecca Hirai <63275270+rebecca1231@users.noreply.github.com>
2020-09-14 06:40:38 +0900

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,29 +5,28 @@ import EmWaterfall from "../EmWaterfall.jpeg";
 import styled from "styled-components";
 
 const Me = styled.div`
-display:flex;
-flex-direction:row;
-margin-top: 5rem;
-`
+  display: flex;
+  flex-direction: row;
+  margin-top: 5rem;
+  justify-content:space-around;
+`;
 
 const About = () => {
   return (
     <Me>
-        <div>
-          <h3 className="ui large header"> Hi,</h3>
-          <h4 className="ui medium header">I build web applications.</h4>
-          <p> </p>
-          </div>
-        <div className="image1">
-          <img
-            className="face"
-            src={myFace}
-            alt="face"
-            style={{ maxWidth: "8rem", borderRadius: "50%" }}
-          />
-        </div>
-
-  
+      <div>
+        <h3 className="ui large header"> Hi,</h3>
+        <h4 className="ui medium header">I build web applications.</h4>
+        <p> I'm always interested in working with positive people on interesting projects. </p>
+      </div>
+      <div className="image1">
+        <img
+          className="face"
+          src={myFace}
+          alt="face"
+          style={{ maxWidth: "8rem", borderRadius: "50%", border:"2px solid #008080" }}
+        />
+      </div>
     </Me>
   );
 };
