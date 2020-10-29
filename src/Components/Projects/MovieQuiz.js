6d5ed414daf3e1b5ee13aca8c71978cc93676efb
Rebecca <rebeccagetsanemail@gmail.com>
2020-10-29 17:16:43 +0900

    edited movie quiz

--- src/Components/Projects/MovieQuiz.js
+++ src/Components/Projects/MovieQuiz.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import dataPoster from "../../projectImages/movieQuiz-data-poster.png";
-import data from "../../projectImages/movieQuiz-data.png";
 import movieQuiz from "../../projectImages/movieQuiz.png";
 import pie from "../../projectImages/movieQuiz-pie.png";
+import quizzes from "../../projectImages/movieQuiz-quizzes.png";
 
 const Container = styled.div`
   max-width: 1000px;
@@ -33,6 +33,7 @@ const Container = styled.div`
 
   img {
     max-width: 28rem;
+    align-self: center;
     box-shadow: 0px 2px 5px #c0c0c0;
     margin: 20px;
   }
@@ -64,16 +65,16 @@ const MovieQuiz = () => {
         <div>
           <h3>Overview</h3>
           <p>
-            Movie Quiz utilizes information from Open IMDb to fetch movie
-            information and injects it into a quiz. I'm currently creating a
-            database connection and authentication routes so users will be able
-            to track their progress.
+            Movie Quiz utilizes information from Open IMDb to fetch information
+            and inject it into a quiz. Users can login to save their quizzes, as
+            well as like and comment on others’.
           </p>
           <h3>Goals</h3>
           <p>
-            I had two goals for this application: to create a dynamic quiz into
-            which a variety of data can easily be injected, and to have the
-            ability to track results.
+            had two goals for this application. First, I wanted to create a fun,
+            dynamic quiz into which any data could easily be injected. Secondly,
+            I really wanted to learn more about GraphQL, especially on the
+            backend.
           </p>
         </div>
         <img src={movieQuiz} alt="quiz" />
@@ -81,45 +82,71 @@ const MovieQuiz = () => {
       <div className="flex">
         <img src={dataPoster} alt="quiz" />
         <div className="distance">
+          <h3>Process</h3>
+          <p>
+            I first created the fun frontend quiz components. I gave them
+            component level state to update with new quiz questions, and to give
+            the user feedback on their answer. In order to keep track of
+            question count and scoring, I created context. It isn’t tied to
+            components and can be accessed not only in the fun quiz, but also by
+            the pie chart to visualize the score. Next, I needed to begin
+            building the backend to retrieve the data for the quiz, and allow
+            users to login, save data, and interact in a lasting way with the
+            application. I didn’t want to repeat the same Express with OAuth
+            that I used in my project Opine. In building Emma’s (Gatsby
+            e-commerce site), I really enjoyed using GraphQl on the frontend.
+            So, I took myself on a journey to learn how to implement GraphQl on
+            the backend with Apollo Server.
+          </p>
           <h3>Bumps Along the Way</h3>
           <p>
-            I created context for the movie and score data, because several
-            components need access to them. In contrast, I also used several
-            component level pieces of state to track local details. In
-            particular, the quiz page consists of two components: one to show
-            the quiz, another to show each answer tile. The tiles need to
-            rerender when selected to give feedback to the user, but the other
-            tiles and the quiz page must remain unchanged.
-          </p>
-          <h3>Lessons Learned</h3>
-          <p>
-            I created the quiz components of this application first. Then, I
-            began creating the backend server and routing. I've deployed the
-            back and front ends separately, but have developed a continuous
-            deployment pipeline for each. I'm learning how to manage these
-            separate deploys and to connect the API securely to the frontend.
+            I was expecting something like Mongoose, but it was more like Redux
+            for the full stack. I didn’t realize how many parts were involved in
+            developing a GraphQl backend until I dived in. It reminds me a
+            little of Redux, with resolvers and type definitions instead of
+            reducers and action creators. GraphQl is strongly-typed and it was
+            my first time to wrap my head around that. Time spent reading error
+            messages and discovering how to operate in the GraphQl type system
+            rewarded me with a reliable server. The front end can retrieve data
+            in exactly the shape it needs and promptly utilize it, rather than
+            retrieving a chunk that has to be reformatted.
           </p>
         </div>
       </div>
-      <h3>Current Work In Progress</h3>
-      <p>
-        I'm really enjoying creating this new project. I'm learning a lot about
-        managing APIs and doing my best to keep the user in mind as I develop
-        new features. I'm working on my data models, database, and routes for
-        common tasks, like saving, retrieving and deleting data. In future, I
-        could possibly add a leaderboard and social media links, so users could
-        share and compare their quiz results.
-      </p>
-
+      <div className="flex">
+        <img src={quizzes} alt="quiz" />
+        <div className="distance">
+          <h3>Lessons Learned</h3>
+          <p>
+            I built Quizzie front to back or in a product-centric manner. It
+            began with the product: what views, interactions, information does
+            it require to be compelling. Then, I took those ideas to the
+            backend, and built out solutions to make them function. For
+            instance, when choosing movie data to load into the quiz, I added a
+            button to show details, like the poster and actor information. It’s
+            not necessary to the functionality of a quiz application, but it
+            allows the user to have a more meaningful interaction with the data.
+            Originally, at the end of the quiz, I simply had the page change to
+            show scores. It felt very abrupt, and I realized, there needed to be
+            a moment of celebration. So, I built some confetti. Engineering
+            Quizzie in a product-centric way has created a better user
+            experience.
+          </p>
+        </div>
+      </div>
       <h3>At a glance</h3>
       <div className="flex">
         <div>
           <h4>Stack</h4>
           <ul>
+            <li>GraphQL</li>
             <li>React</li>
             <li>D3</li>
             <li>Node</li>
-            <li>Express</li>
+            <li>Apollo Server</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>Styled Components</li>
           </ul>
         </div>
         <div>
@@ -146,13 +173,12 @@ const MovieQuiz = () => {
           </ul>
         </div>
         <img
-          src={data}
-          alt="form"
           className="mobile-image"
+          src={pie}
+          alt="form"
           style={{ margin: 0 }}
         />
       </div>
-      <img src={pie} alt="" />
     </Container>
   );
 };
