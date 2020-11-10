f5b446ea2c7a12d56be81073e4d9a6e03da2117a
Rebecca <rebeccagetsanemail@gmail.com>
2020-11-10 12:17:50 +0900

    edited about content

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { gsap } from "gsap";
+import { useHistory } from "react-router-dom";
 
 import wasabi from "../wasabi.jpg";
 
@@ -10,10 +11,20 @@ const Container = styled.div`
   padding: 1rem;
   line-height: 1.5;
   font-size: 1.25rem;
-
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+
+  span{
+    color: hsla(0, 0%, 0%, 0.8);
+  }
+
+  span:hover{
+    cursor:pointer;
+    text-decoration: underline;
+    color:#008891;
+
+  }
 `;
 
 const Me = styled.div`
@@ -52,14 +63,16 @@ const Me = styled.div`
 `;
 
 const About = () => {
+  const history = useHistory();
+
   const topRef = useRef(null);
   useEffect(() => {
     gsap.from(topRef.current, {
       duration: 1,
       autoAlpha: 0,
-      x: 50,
+      y: 20,
+      x:20
     });
-    
   }, []);
 
   return (
@@ -81,11 +94,10 @@ const About = () => {
       <div>
         <p>
           My first experience with development was when I was 13. I made my own
-          web page (with all the bad poetry) and had a lot of fun creating
-          styles. I would look at the source code of other sites and dissect how
-          they worked. I used that knowledge to make a hover effect with a
-          racing rainbow background and bold white text that I was very proud of
-          at the time.
+          web page and had a lot of fun creating styles. I would look at the
+          source code of other sites and dissect how they worked. I used that
+          knowledge to make a hover effect with a racing rainbow background and
+          bold white text that I was very proud of at the time.
         </p>
         <p>
           Years later, I decided to try learning JavaScript and got hooked. I
@@ -103,17 +115,16 @@ const About = () => {
           happen. JavaScript is like a superpower.
         </p>
         <p>
-          I love to learn and grow. My latest project is a JamStack application,
-          using Gatsby. I'm building a customized headless CMS, currently
-          deployed on heroku. It is still a work in progress, but you can check
-          out the application as is {" "}
-          <a
+          I love to learn and grow. My latest project is a full stack quiz
+          application. It consumes outside API data to dynamically create fun
+          quizzes. I built it with GraphQL, Node, and React. Please check it out {" "}
+          <span
             target="_blank"
             rel="noopener noreferrer"
-            href="https://emmas-boutique.netlify.app"
+            onClick={() => history.push("/projects/movie-quiz")}
           >
             here
-          </a>
+          </span>
           .
         </p>
         <p>
